90bcd292cdfa2e9cdf57c25bbdce0727fe6d7a89
Jorge S. Sanchez <georotzen@gmail.com>
2021-11-08 16:22:52 -0800

    Initial commit on November 8, 2021

---
+++ bookEx/bookMng/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
+    path('postbook', views.postbook, name='postbook'),
+    path('displaybooks', views.displaybooks, name='displaybooks'),
+    path('mybooks', views.mybooks, name='mybooks'),
+    path('search', views.search_book, name='search'),
+    path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
+]
